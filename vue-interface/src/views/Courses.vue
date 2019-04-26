c7195c16ab9aa1670761ae5538ee94006e1c7dc3
Mike <tallinn@gmail.com>
2019-04-26 14:23:35 +0300

    Courses filter works

--- vue-interface/src/views/Courses.vue
+++ vue-interface/src/views/Courses.vue
@@ -9,171 +9,26 @@
     <div class="row zero_side_margin">
       <div class="col-lg-12 main_block">
         <div class="btn-group page_courses__courses_groups d-flex" role="group">
-          <a href class="btn btn-outline-primary active w-100">All</a>
-          <a href class="btn btn-outline-primary w-100">Programming</a>
-          <a href class="btn btn-outline-primary w-100">Web-design</a>
-          <a href class="btn btn-outline-primary w-100">Game development</a>
+          <router-link
+            v-for="group in groups"
+            :key="group.id"
+            to="/courses"
+            v-on:click.native="filter_courses(group.number)"
+            class="btn btn-outline-primary w-100"
+            :class="{ 'active' : current_filter === group.number }"
+          >{{group.name}}</router-link>
         </div>
       </div>
     </div>
 
     <div class="row zero_side_margin">
       <div class="col-md-12 main_block">
-        <!-- <div class="card border-secondary page_courses__course_card">
-          <div class="row zero_side_margin align-items-center">
-            <div class="col-md-3 page_courses__course_card_image">
-              <img src="../assets/programming.svg" width="200" class="img-fluid" alt>
-            </div>
-            <div class="col-md-9 page_courses__course_card_info">
-              <div class="row zero_side_margin">
-                <div class="col-md-12">
-                  <h4
-                    class="card-title page_courses__course_card_info_title"
-                  >Introduction to C# Programming</h4>
-                  <p class="card-text">First steps into the big world of programming</p>
-                </div>
-              </div>
-              <div class="row zero_side_margin page_courses__course_card_info_details">
-                <div class="col-md-6">
-                  <p>
-                    <i
-                      class="fa fa-calendar fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>16 weeks - from April
-                    to June
-                  </p>
-                  <p>
-                    <i
-                      class="fa fa-clock-o fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>Twice a week on Mondays
-                    and Fridays
-                  </p>
-                </div>
-                <div class="col-md-6">
-                  <p>
-                    <i
-                      class="fa fa-user fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>8-12 years
-                  </p>
-                  <p>
-                    <i
-                      class="fa fa-money fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>200 EUR / course
-                  </p>
-                </div>
-              </div>
-              <div class="row zero_side_margin page_courses__course_card_info_register_row">
-                <div class="col-md-12 page_courses__course_card_info_register_row_button">
-                  <a href="register.html" class="btn btn-primary">Register</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="card page_courses__course_card border-secondary">
-          <div class="row zero_side_margin align-items-center">
-            <div class="col-md-3 page_courses__course_card_image">
-              <img src="../assets/web-programming.svg" width="200" class="img-fluid" alt>
-            </div>
-            <div class="col-md-9 page_courses__course_card_info">
-              <div class="row zero_side_margin">
-                <div class="col-md-12">
-                  <h4 class="card-title page_courses__course_card_info_title">Web-design</h4>
-                  <p
-                    class="card-text"
-                  >Learn how to create and design the web-sites. From basics in HTML to CSS.</p>
-                </div>
-              </div>
-              <div class="row zero_side_margin page_courses__course_card_info_details">
-                <div class="col-md-6">
-                  <p>
-                    <i
-                      class="fa fa-calendar fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>10 weeks - from March to May
-                  </p>
-                  <p>
-                    <i
-                      class="fa fa-clock-o fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>On Sundays
-                  </p>
-                </div>
-                <div class="col-md-6">
-                  <p>
-                    <i
-                      class="fa fa-user fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>8-12 years
-                  </p>
-                  <p>
-                    <i
-                      class="fa fa-money fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>200 EUR / course
-                  </p>
-                </div>
-              </div>
-              <div class="row zero_side_margin page_courses__course_card_info_register_row">
-                <div class="col-md-12 page_courses__course_card_info_register_row_button">
-                  <a href="register.html" class="btn btn-primary">Register</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="card page_courses__course_card border-secondary page_courses__course_card">
-          <div class="row zero_side_margin align-items-center">
-            <div class="col-md-3 page_courses__course_card_image">
-              <img src="../assets/game-development.svg" width="200" class="img-fluid" alt>
-            </div>
-            <div class="col-md-9 page_courses__course_card_info">
-              <div class="row zero_side_margin">
-                <div class="col-md-12">
-                  <h4
-                    class="card-title page_courses__course_card_info_title"
-                  >Game developement with Unity Engine</h4>
-                  <p class="card-text">Design your first real video game</p>
-                </div>
-              </div>
-              <div class="row zero_side_margin page_courses__course_card_info_details">
-                <div class="col-md-6">
-                  <p>
-                    <i
-                      class="fa fa-calendar fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>32 weeks - from April to September
-                  </p>
-                  <p>
-                    <i
-                      class="fa fa-clock-o fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>On Wednesdays
-                  </p>
-                </div>
-                <div class="col-md-6">
-                  <p>
-                    <i
-                      class="fa fa-user fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>13-16 years
-                  </p>
-                  <p>
-                    <i
-                      class="fa fa-money fa-fw page_courses__course_card_info_details--separate-item"
-                    ></i>400 EUR / course
-                  </p>
-                </div>
-              </div>
-              <div class="row zero_side_margin page_courses__course_card_info_register_row">
-                <div class="col-md-12 page_courses__course_card_info_register_row_button">
-                  <a href="register.html" class="btn btn-primary">Register</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>-->
-
         <!-- <a class="btn btn-primary" @click="test_function" role="button">Test</a> -->
 
         <div
           class="card page_courses__course_card border-secondary page_courses__course_card"
           :key="course.id"
-          v-for="course in courses_data"
+          v-for="course in courses_to_view"
         >
           <div class="row zero_side_margin align-items-center">
             <div class="col-md-3 page_courses__course_card_image">
@@ -239,21 +94,41 @@
 </template>
 
 <script>
-import json from "../assets/courses.json";
+import coursesJson from "../assets/courses.json";
+import groupsJson from "../assets/groups.json";
 
 export default {
   name: "Courses",
   data() {
     return {
-      courses_data: json
-    }
+      courses_data: coursesJson,
+      groups: groupsJson,
+      courses_to_view: [],
+      current_filter: 0
+    };
   },
   methods: {
     getAssetsPath(filename) {
       return require("../assets/" + filename);
+    },
+    filter_courses(groupNum) {
+      this.current_filter = groupNum;
+      if (groupNum === 0) {
+        this.courses_to_view = this.courses_data;
+      } else {
+        let filtered_list = [];
+        for (let key in this.courses_data) {
+          let course = this.courses_data[key];
+          if (course.group === groupNum) {
+            filtered_list.push(course);
+          }
+        }
+        this.courses_to_view = filtered_list;
+      }
     }
   },
-  // mounted() {
-  // }
+  mounted() {
+    this.courses_to_view = this.courses_data;
+  }
 };
 </script>
