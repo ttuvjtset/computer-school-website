dff4c3c1fb160232179f22d37b6faa859e7fc608
Mike <tallinn@gmail.com>
2019-04-26 16:12:14 +0300

    Implemented error if no course found

--- vue-interface/src/views/Register.vue
+++ vue-interface/src/views/Register.vue
@@ -1,107 +1,118 @@
 <template>
   <div class="page_register">
-    <div class="row zero_side_margin">
-      <div class="col-md-12 main_block">
-        <h4 class="main_block__page_header">Register for the course</h4>
+    <div v-if="no_course_found" class="row zero_side_margin">
+      <div class="col-md-12 main_block main_block__error">
+        <div class="alert alert-warning" role="alert">
+          No such course found. Please find a suitable course on the
+          <router-link to="/courses" class="alert-link">courses page</router-link>.
+        </div>
       </div>
     </div>
 
-    <div class="row page_register_block zero_side_margin align-items-center">
-      <div class="col-md-8 main_block">
-        <div class="row page_register_block__course align-items-center">
-          <div class="col-md-4 page_register_block__course--text col-lg-3">Selected Course</div>
-          <div class="col-md-8 col-lg-9">
-            <img
-              :src="getAssetsPath(selected_course.img_src)"
-              width="45"
-              class="img page_register_block__course--image"
-              alt
-            >
-            {{selected_course.title}}
-          </div>
-        </div>
-
-        <div class="row align-items-center">
-          <div class="col-md-12">
-            <form>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="inputParentFirstName">Parent Name</label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="inputParentFirstName"
-                    placeholder="First Name"
-                  >
-                </div>
-                <div class="form-group col-md-6">
-                  <label
-                    class="page_register_block__form--hidden_label"
-                    for="inputParentLastName"
-                  >&nbsp;</label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="inputParentLastName"
-                    placeholder="Last Name"
-                  >
-                </div>
-              </div>
-
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="inputStudentFirstName">Student Name</label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="inputStudentFirstName"
-                    placeholder="First Name"
-                  >
-                </div>
-                <div class="form-group col-md-6">
-                  <label
-                    class="page_register_block__form--hidden_label"
-                    for="inputStudentLastName"
-                  >&nbsp;</label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="inputStudentLastName"
-                    placeholder="Last Name"
-                  >
-                </div>
-              </div>
-
-              <div class="form-group">
-                <label for="telephone">Telephone</label>
-                <input type="text" class="form-control" id="telephone" placeholder="Telephone">
-              </div>
-
-              <div class="form-group">
-                <label for="telephone">E-mail</label>
-                <input type="text" class="form-control" id="email" placeholder="E-mail">
-              </div>
-
-              <div class="page_register_block__form_button_row">
-                <router-link
-                  class="btn page_register_block__form_button_row--button btn-primary"
-                  to="/thankyou"
-                  role="button"
-                >Register</router-link>
-              </div>
-            </form>
-          </div>
+    <template v-if="!no_course_found">
+      <div class="row zero_side_margin">
+        <div class="col-md-12 main_block">
+          <h4 class="main_block__page_header">Register for the course</h4>
         </div>
       </div>
 
-      <div class="col-md-4 page_register_block__image main_block">
-        <img
-          :src="getAssetsPath('002-immersive.svg')"
-          class="img-fluid page_register_block__image--smaller"
-          alt
-        >
+      <div class="row page_register_block zero_side_margin align-items-center">
+        <div class="col-md-8 main_block">
+          <div class="row page_register_block__course align-items-center">
+            <div class="col-md-4 page_register_block__course--text col-lg-3">Selected Course</div>
+            <div class="col-md-8 col-lg-9">
+              <img
+                :src="getAssetsPath(selected_course.img_src)"
+                width="45"
+                class="img page_register_block__course--image"
+                alt
+              >
+              {{selected_course.title}}
+            </div>
+          </div>
+
+          <div class="row align-items-center">
+            <div class="col-md-12">
+              <form>
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputParentFirstName">Parent Name</label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="inputParentFirstName"
+                      placeholder="First Name"
+                    >
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label
+                      class="page_register_block__form--hidden_label"
+                      for="inputParentLastName"
+                    >&nbsp;</label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="inputParentLastName"
+                      placeholder="Last Name"
+                    >
+                  </div>
+                </div>
+
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputStudentFirstName">Student Name</label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="inputStudentFirstName"
+                      placeholder="First Name"
+                    >
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label
+                      class="page_register_block__form--hidden_label"
+                      for="inputStudentLastName"
+                    >&nbsp;</label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="inputStudentLastName"
+                      placeholder="Last Name"
+                    >
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="telephone">Telephone</label>
+                  <input type="text" class="form-control" id="telephone" placeholder="Telephone">
+                </div>
+
+                <div class="form-group">
+                  <label for="telephone">E-mail</label>
+                  <input type="text" class="form-control" id="email" placeholder="E-mail">
+                </div>
+
+                <div class="page_register_block__form_button_row">
+                  <router-link
+                    class="btn page_register_block__form_button_row--button btn-primary"
+                    to="/thankyou"
+                    role="button"
+                  >Register</router-link>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 page_register_block__image main_block">
+          <img
+            :src="getAssetsPath('002-immersive.svg')"
+            class="img-fluid page_register_block__image--smaller"
+            alt
+          >
+        </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -113,7 +124,8 @@ export default {
   data() {
     return {
       courses_data: coursesJson,
-      selected_course: {}
+      selected_course: {},
+      no_course_found: true
     };
   },
   components: {},
@@ -126,6 +138,7 @@ export default {
         let course = this.courses_data[key];
         if (course.id === parseInt(courseNum)) {
           this.selected_course = course;
+          this.no_course_found = false;
         }
       }
     }
@@ -135,3 +148,9 @@ export default {
   }
 };
 </script>
+
+<style scope>
+.main_block__error {
+  padding-bottom: 400px;
+}
+</style>
